5ba1c23014f297e196f15934cb00916c8b73fb1d
= <=>
2020-08-23 18:00:42 +0800

    Duser active_for_authentication?

--- app/models/duser.rb
+++ app/models/duser.rb
@@ -5,6 +5,23 @@ class Duser < ApplicationRecord
          :recoverable, :rememberable, :validatable
   enum group: {sysadmin:0, orderadmin:1, worker:2}
   has_and_belongs_to_many :roles, -> { alphabetical }
+  
+  def active_for_authentication?
+    super && !self.planlimitreached?
+  end
+  
+  def inactive_message
+    "Sorry, this plan limit has been reached."
+  end
+
+  def planlimitreached?
+    if Location.all.size >= id.to_i
+      true
+    else
+      false
+    end
+  end
+
   def sysadmin?
     if self.roles.exists?(name: "sysadmin")
       true
