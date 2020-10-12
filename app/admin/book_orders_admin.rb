741df63bdc1944284873725565064cfd9f6aa9ce
linhaishen <linhai.shen@outlook.com>
2020-10-12 17:58:07 +0800

    select :status, BookOrder.statuses.keys

--- app/admin/book_orders_admin.rb
+++ app/admin/book_orders_admin.rb
@@ -30,7 +30,7 @@ Trestle.resource(:book_orders) do
       row do
         text_field :id,:readonly => true
         text_field :refnumber
-        select :status, BookOrder::STATUSES
+        select :status, BookOrder.statuses.keys
        select :assignee_id, Duser.all, include_blank: true
       end
       text_field :deliveryaddress
